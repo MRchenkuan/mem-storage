61d0c344805bee39a736c16a0d437c6393d93624
chenkuan <393667111@qq.com>
2019-02-09 00:26:20 +0800

    commet

--- src/IStorage.ts
+++ src/IStorage.ts
@@ -1,7 +1,29 @@
+/**
+ * 通用 Storage 接口
+ */
 export default interface IStorage {
+
+    /**
+     * 批量储存 localStorage
+     * @param obj 批量储存对象
+     */
     set(obj: Object):Promise<any>;
+
+    /**
+     * 储存指定的 storage
+     * @param key key
+     * @param val 值
+     */
     set(key:string, val: any):Promise<any>;
+
+    /**
+     * 获取 storage
+     * @param key storage 名称
+     */
     get(key: string): Promise<any>;
-    getLocalStorage(): Promise<any>;
+
+    /**
+     * 获取本地已有的 storage
+     */
     getLocalStorage(): Promise<any>;
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,7 @@
 export default {
+    /**
+     * 鉴定是否处于小程序环境
+     */
     isWxApplet() {
         if (typeof wx !=='object') return false;
         if (!wx.setStorage) return false;
