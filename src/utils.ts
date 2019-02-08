1094ccbf26f4702d7904d076457b581be72ae039
chenkuan <393667111@qq.com>
2019-02-08 16:14:42 +0800

    update

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,9 @@
 export default {
     isWxApplet() {
         if (typeof wx !=='object') return false;
-        if (!wx.navigateTo) return false;
-        if (!wx.redirectTo) return false;
-        if (!wx.navigateBack) return false;
-        if (typeof getCurrentPages !== 'function') return false;
+        if (!wx.setStorage) return false;
+        if (!wx.getStorage) return false;
+        if (!wx.getStorageInfo) return false;
         return true;
     }
 }
